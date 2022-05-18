78b8dff47a3e2d06da48425879fef70e4f7c305b
Dipen <dipendangol1998@gmail.com>
2022-05-18 14:55:52 +0545

    removed comment outs

--- src/pages/ListTransaction.js
+++ src/pages/ListTransaction.js
@@ -10,40 +10,9 @@ import {
 } from "@mui/material";
 import axios from "axios";
 import "../App.css";
-// import useRefreshToken from "../hooks/useRefreshToken";
-// import jwt_decode from "jwt-decode";
 
 const ListTransactionPage = () => {
   const [apiData, setApiData] = useState([]);
-  // const refresh = useRefreshToken();
-
-  // const refreshToken = async () => {
-  //   try {
-  //     const res = await axios.post("http://localhost:3500/refresh", {
-  //       token: localStorage.getItem("refreshToken"),
-  //     });
-  //     localStorage.setItem("accessToken", res.data.accessToken);
-  //     localStorage.setItem("refreshToken", res.data.refreshToken);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // axios.interceptors.request.use(
-  //   async (config) => {
-  //     let currentDate = new Date();
-  //     const decodedToken = jwt_decode(localStorage.getItem("accessToken"));
-  //     if (decodedToken.exp * 1000 < currentDate.getTime()) {
-  //       await refreshToken();
-  //       config.headers.authorization =
-  //         "Bearer " + localStorage.getItem("accessToken");
-  //     }
-  //     return config;
-  //   },
-  //   (err) => {
-  //     return Promise.reject(err);
-  //   }
-  // );
 
   useEffect(() => {
     axios
